e2e865f2d20217fed1ff1eed6a9787445b76af46
Deepumandal <nomercyhunters@gmail.com>
2022-10-23 16:32:12 +0000

    Deploying to public from @ Deepumandal/next-js-gh-pages-deploy@2de53f7758079ba31774231e4d4c246aa0a17616 🚀

--- _next/static/07mnsNyM979pOFs-C8Y7M/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-390cd69ca5f42d9a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
